0634dd018434d9927f54ec1da1b2ec0041f1c379
taekhyang <xorgid1195@gmail.com>
2021-04-13 18:07:17 +0900

    WIP

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -11,7 +11,7 @@ const signUp = async (req, res, next) => {
     try {
         const { email, password, userType, username } = req.body
 
-        const foundUser = await User.findByEmail(email)
+        const foundUser = User.findByEmail(email)
         if (foundUser) {
             return res.status(400).json({
                 message: 'EMAIL_ALREADY_EXIST'
@@ -30,7 +30,44 @@ const signUp = async (req, res, next) => {
     }
 }
 
+const logIn = async (req, res, next) => {
+    try {
+        const { email, password: inputPassword } = req.body
+
+        const foundUser = await User.findByEmail(email)
+
+        if (!foundUser) {
+            return res.status(404).json({
+                message: 'USER_DOES_NOT_EXIST'
+            })
+        }
+
+
+        const { id, password: hashedPassword } = foundUser
+        console.log(inputPassword, hashedPassword)
+        console.log(foundUser)
+        const isValidPassword = await bcrypt.compare(inputPassword, hashedPassword)
+
+        if (!isValidPassword) {
+            return res.status(404).json({
+                message: 'INVAILD_PASSWORD'
+            })
+        }
+
+        const token = foundUser.generateAuthToken()
+        return res.status(200).json({
+            message: 'LOGIN_SUCCESS',
+            token
+        })
+
+
+    } catch (err) {
+        next(err)
+    }
+}
+
 
 router.post('/signup', signUp)
+router.post('/login', logIn)
 
 module.exports = router
